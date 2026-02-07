8d89cc7ecb3fd99bad114104d94f659da72cf823
Jimmy <jphalen335@comcast.net>
2026-02-07 10:31:15 -0500

    add convert_temperature.py

---
+++ convert_temperature.py
@@ -0,0 +1,44 @@
+# ### Task 1.2: Create convert_temperature() function
+# Write a function using the math module.
+
+# **Requirements:**
+# - Import the `math` module
+# - Function name: `convert_temperature(celsius)`
+# - Convert Celsius to Fahrenheit using formula: `F = C * 9/5 + 32`
+# - Use `math.floor()` to return integer result
+# - Include docstring
+
+# **Learn About:**
+# - Importing modules: `import math`
+# - Using module functions: `math.floor(value)`
+# - The `dir(math)` function to see available functions
+# - The `help(math.floor)` function for documentation
+
+# ---
+
+import math
+
+
+def convert_temperature(celsius):
+    """Convert Celsius to Fahrenheit and return a floored integer value.
+    Args:
+        celsius (float): Temperature in degrees Celsius
+    Returns:
+        int: Temperature in degrees Fahrenheit (floored by math)
+    """
+
+    try:
+        celsius = float(celsius)
+    except (TypeError, ValueError):
+        raise ValueError("Temperature input must in digits")
+
+    fahrenheit = (celsius * (9 / 5)) + 32  # Yes I know (()) not needed but it helps show ooo.
+    return math.floor(fahrenheit)
+
+
+try:
+    print(convert_temperature(0))  # 32
+    print(convert_temperature(25))  # 77
+    print(convert_temperature(36.6))  # 97
+except ValueError as e:
+    print(f"ValueError: {e}")
