96efc7bdbd48d9a27240ff6c371be9674c0fc7da
Jimmy <jphalen335@comcast.net>
2026-02-14 10:33:24 -0500

    updated scratch to tax question

--- scratch.py
+++ scratch.py
@@ -1,4 +1,12 @@
-numbers = [int(input("Enter the first number: ")), int(input("Enter the second number: ")), int(input("Enter the third number: "))]
-largest_number = max(numbers)
+income = float(input("Enter your annual income: "))
 
-print(f"The largest number is: {largest_number}")
+if income > 85528:
+    tax = 14839.02 + (0.32 * (income - 85528))
+else:
+    tax = (0.18 * income) - 556.02
+
+if tax < 0:
+    tax = 0
+
+tax = round(tax, 0)
+print(f"Your tax is: {tax:.2f}", tax, "thalers")
