9b84c908a89514a25e0861e87382febc383016ea
Jimmy <jphalen335@comcast.net>
2026-02-14 11:23:03 -0500

    fix: years.py check that year is in greg cal

--- years.py
+++ years.py
@@ -4,7 +4,9 @@ except ValueError:
     print("Invalid input. Please enter a numeric value for the year.")
     exit(1)
 
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+if year <= 1582:
+    print("Not within the Gregorian calendar period.")
+elif (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(f"{year} is a leap year.")
 else:
     print(f"{year} is a common year.")
